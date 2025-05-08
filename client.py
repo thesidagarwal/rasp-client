a545d179164016cb7c084cbd90a7c9aade75dd34
Sid Agarwal <thesidagarwal@gmail.com>
2025-05-08 13:09:02 +0530

    Adding logs and alerts

--- client.py
+++ client.py
@@ -1,19 +1,23 @@
-# client.py on Raspberry Pi
+
 import requests
 import time
 from datetime import datetime
 import pytz
+import os
 
+TIMEZONE_IST = pytz.timezone('Asia/Kolkata')
 SERVER_IP = 'http://91.99.6.15:8001/status/allen'
 REQUEST_TIMEOUT = 2
 SLEEP_TIME = 1
 
 def get_status():
     try:
+        os.system('clear')
+        
         response = requests.get(SERVER_IP, timeout=REQUEST_TIMEOUT)
         response.raise_for_status()
         data = response.json()
-        
+
         # Parse the UTC expiry timestamp
         utc_time = datetime.fromisoformat(data["expires"].replace("Z", "+00:00"))
 
@@ -22,11 +26,19 @@ def get_status():
 
         print(f"Status: {data['status']}")
         print(f"🕒 Expires at (IST): {ist_time.strftime('%Y-%m-%d %H:%M:%S')}")
+
+        if data['status'] != "true":
+            print("Locked!")
+            print('\a')
     except requests.exceptions.HTTPError as err:
         if response.status_code == 404:
             print("❌ Status not found")
+            print("Locked!")
+            print('\a')
         elif response.status_code == 410:
             print("⚠️ Status expired")
+            print("Locked!")
+            print('\a')
         else:
             print("❌ Server error:", err)
     except Exception as e:
@@ -35,4 +47,4 @@ def get_status():
 if __name__ == "__main__":
     while True:
         get_status()
-        time.sleep(SLEEP_TIME)  
+        time.sleep(SLEEP_TIME)
